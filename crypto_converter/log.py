04bcabc4646b60e6a26bae750d68ed9029a49256
devintensify <ilia.naidenov.98@gmail.com>
2025-07-22 18:35:15 +0400

    fix args formatter

--- crypto_converter/log.py
+++ crypto_converter/log.py
@@ -52,14 +52,12 @@ def log_awaitable_method(
         logger = logging.getLogger(logger_name)
 
         async def _method_wrapper(self: _T, *args: _P.args, **kwargs: _P.kwargs) -> _R:
-            method_name = method.__name__
+            to_log_args, to_log_kwargs = _get_args_kwargs_to_log(
+                args, kwargs, log_kwargs=log_kwargs
+            )
+            method_repr = _get_method_repr(method, to_log_args, to_log_kwargs)
             object_repr = self.__class__.__name__ if not use_class_repr else repr(self)
-            method_repr = f"`{method_name}` method of `{object_repr}`"
-            if log_kwargs:
-                formatted_log_kwargs = [
-                    f"{kwarg}=`{kwargs.get(kwarg)}`" for kwarg in log_kwargs
-                ]
-                method_repr += f" ({', '.join(formatted_log_kwargs)})"
+            method_repr = f"`{method_repr}` method of `{object_repr}`"
 
             attempt_logger = getattr(logger, level_on_attempt)
             attempt_logger("Calling %s..", method_repr)
@@ -110,28 +108,58 @@ def log_method(
         logger = logging.getLogger(logger_name)
 
         def _method_wrapper(self: _T, *args: _P.args, **kwargs: _P.kwargs) -> _R:
-            method_name = method.__name__
+            to_log_args, to_log_kwargs = _get_args_kwargs_to_log(
+                args, kwargs, log_kwargs=log_kwargs
+            )
+            method_repr = _get_method_repr(method, to_log_args, to_log_kwargs)
             object_repr = self.__class__.__name__ if not use_class_repr else repr(self)
-            method_repr = f"`{method_name}` method of `{object_repr}`"
-            if log_kwargs:
-                formatted_log_kwargs = [
-                    f"{kwarg}=`{kwargs.get(kwarg)}`" for kwarg in log_kwargs
-                ]
-                method_repr += f" ({', '.join(formatted_log_kwargs)})"
+            method_repr = f"`{method_repr}` method of `{object_repr}`"
 
             attempt_logger = getattr(logger, level_on_attempt)
             attempt_logger("Calling %s..", method_repr)
             try:
                 result = method(self, *args, **kwargs)
-                attempt_logger("%s: succeeded", method_repr)
             except Exception as exc:
                 exc_repr = f"Type={type(exc)}, Args={exc.args}, Message={exc}"
                 error_logger = getattr(logger, level_on_error)
                 error_logger("%s: failed with error: `%s`", method_repr, exc_repr)
                 raise
             else:
+                attempt_logger("%s: succeeded", method_repr)
                 return result
 
         return _method_wrapper
 
     return _log_method
+
+
+def _get_args_kwargs_to_log(
+    args: Any, kwargs: Any, log_kwargs: list[str] | None = None
+) -> tuple[list[Any], dict[str, Any]]:
+    list_args = list(args) if args else []
+    dict_kwargs = dict(kwargs) if kwargs else {}
+    filtered_dict_kwargs = (
+        dict_kwargs
+        if log_kwargs is None
+        else {name: value for name, value in dict_kwargs.items() if name in log_kwargs}
+    )
+    return list_args, filtered_dict_kwargs
+
+
+def _get_method_repr(
+    method: _MethodType | _AsyncMethodType,
+    args: list[Any],
+    kwargs: dict[str, Any],
+) -> str:
+    method_repr = method.__name__
+    method_content = []
+    if args:
+        method_content.append(", ".join(list(map(str, args))))
+    if kwargs:
+        method_content.append(
+            ", ".join([f"{name}={arg}" for name, arg in kwargs.items()])
+        )
+    if method_content:
+        method_repr += "(" + ", ".join(method_content) + ")"
+
+    return method_repr
